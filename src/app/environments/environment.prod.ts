0b2b03ce98518951f23e7cfc6fc9b672dc88ee29
Jaime Benalcazar <jaimeb_2006@hotmail.com>
2024-08-12 19:41:24 -0500

    Cambio de hosting

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,12 +1,4 @@
 export const environment = {
-  firebase: {
-    apiKey: 'AIzaSyALBFY6JBfd_EE2Rgr7YU9EZqV8J7nPo2Q',
-    authDomain: 'angular-neoris-tech-test.firebaseapp.com',
-    projectId: 'angular-neoris-tech-test',
-    storageBucket: 'angular-neoris-tech-test.appspot.com',
-    messagingSenderId: '939354191039',
-    appId: '1:939354191039:web:3a1809cc3118c38d81420a',
-  },
   production: true,
   apiBaseUrl:
     'https://tribu-ti-staffing-desarrollo-afangwbmcrhucqfh.z01.azurefd.net/ipf-msa-productosfinancieros/bp/products',
